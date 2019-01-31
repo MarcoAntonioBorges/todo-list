6fceaf57847e7a9113cc50a72edf2119a5da9f76
MarcoAntonioBorges <mabo.marcoantonio@gmail.com>
2019-01-31 16:07:04 -0200

    Update files

--- _00 Meus Aquivos/myApp/myProjectApp/src/components/HelloWorld.vue
+++ _00 Meus Aquivos/myApp/myProjectApp/src/components/HelloWorld.vue
@@ -4,21 +4,3 @@
     <v-btn color="success">Success</v-btn>
   </div>
 </template>
-
-<script>
-import 'vuetify/dist/vuetify.min.css'
-
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
