7687c90bb870622a42a322ceaad16d5a5009d62c
MarcoAntonioBorges <mabo.marcoantonio@gmail.com>
2019-02-18 18:56:13 -0300

    Criando a pagina de Listar tarefas

--- _00 Meus Aquivos/myApp/myProjectApp/src/components/ListarTarefas.vue
+++ _00 Meus Aquivos/myApp/myProjectApp/src/components/ListarTarefas.vue
@@ -1,79 +1,40 @@
 <template>
-  <v-layout xs12 sm6 md12 row wrap align-start justify-around class="ml-4">
-        <v-flex xs12 md4 class="mb-4">
-            <v-card>
-                <div class="white--text" style="height:100px; background-color: red;">
-                    <v-container fill-height fluid>
-                        <v-layout fill-height>
-                            <v-flex xs12 align-end flexbox>
-                                <span class="headline">[ Nome da Tarefa ]</span>
-                            </v-flex>
-                        </v-layout>
-                    </v-container>
-                </div>
-                <v-card-title>
-                    <div>
-                        <span>[ Descrição da Tarefa ]</span>
-                        <br>
-                        <span class="grey--text">[ Data da Tarefa ]</span>
-                    </div>
-                </v-card-title>
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn flat color="orange">Editar</v-btn>
-                    <v-btn flat color="green">Finalizar</v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-flex>
-        <v-flex xs12 md4 class="mb-4">
-            <v-card>
-                <div class="white--text" style="height:100px; background-color: yellow;">
-                    <v-container fill-height fluid>
-                        <v-layout fill-height>
-                            <v-flex xs12 align-end flexbox>
-                                <span class="headline">[ Nome da Tarefa ]</span>
-                            </v-flex>
-                        </v-layout>
-                    </v-container>
-                </div>
-                <v-card-title>
-                    <div>
-                        <span>[ Descrição da Tarefa ]</span>
-                        <br>
-                        <span class="grey--text">[ Data da Tarefa ]</span>
-                    </div>
-                </v-card-title>
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn flat color="orange">Editar</v-btn>
-                    <v-btn flat color="green">Finalizar</v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-flex>
-        <v-flex xs12 md4 class="mb-4">
-            <v-card>
-                <div class="white--text" style="height:100px; background-color: blue;">
-                    <v-container fill-height fluid>
-                        <v-layout fill-height>
-                            <v-flex xs12 align-end flexbox>
-                                <span class="headline">[ Nome da Tarefa ]</span>
-                            </v-flex>
-                        </v-layout>
-                    </v-container>
-                </div>
-                <v-card-title>
-                    <div>
-                        <span>[ Descrição da Tarefa ]</span>
-                        <br>
-                        <span class="grey--text">[ Data da Tarefa ]</span>
-                    </div>
-                </v-card-title>
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn flat color="orange">Editar</v-btn>
-                    <v-btn flat color="green">Finalizar</v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-flex>
+  <v-layout xs12 sm6 md12 row wrap style="padding:15px; justify-content: space-between; align-items: flex-start; align-content: flex-start">
+    <v-flex xs12 md5 class="mb-5" v-for="tarefa in tarefas" :key="tarefa.id">
+      <v-card color="teal lighten-1">
+        <div class="dark--text" style="background-color: white;">
+          <v-container fill-height fluid>
+            <v-layout fill-height>
+              <v-flex xs12 align-end flexbox>
+                <span class="headline">[ {{ tarefa.nomeTarefa }} ]</span>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </div>
+        <v-card-title>
+          <div class="white--text">
+            <span>[ {{ tarefa.descTarefa }} ]</span>
+            <br>
+            <span>[ Data da Tarefa ]</span>
+          </div>
+        </v-card-title>
+        <v-card-actions>
+          <v-btn flat color="red">Editar</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn color="indigo darken-4" outline>Finalizar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
   </v-layout>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    tarefas: [
+      {id: 1, nomeTarefa: 'Primeira tarefa', descTarefa: 'desc tarefa', style: 'height: 100px; background-color: blue'},
+      {id: 2, nomeTarefa: 'Segunda tarefa', descTarefa: 'Segunda desc tarefa', style: 'height: 100px; background-color: yellow'}
+    ]
+  })
+}
+</script>
